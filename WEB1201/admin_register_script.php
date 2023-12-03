8cb8589445c3339eec50a654cd5e582a3de0b50e
Sean Yee <50612868+SeanYee52@users.noreply.github.com>
2023-12-03 09:11:03 +0000

    new file:   WEB1201/admin_register_script.php 	new file:   WEB1201/loggedin.php 	modified:   WEB1201/login.php 	modified:   WEB1201/login_functions.inc.php 	modified:   WEB1201/register_script.php     Updated and revamped login and registration process, now works for both users and admin.     TO DO:     1. Determine where will the user be redirected to after log inc     2. Ensure user can't log in multiple times     3. Ensure that users can't log into a normal account and an admin accounta at the same time (Can be resolved by resetting the session whenever the user goes back to the login page)

--- WEB1201/admin_register_script.php
+++ WEB1201/admin_register_script.php
@@ -17,13 +17,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$fn = mysqli_real_escape_string($dbc, trim($_POST['username']));
 	}
 
-	// Check for an email address:
-	if (empty($_POST['email'])) {
-		$errors[] = 'You forgot to enter your email.';
-	} else {
-		$e = mysqli_real_escape_string($dbc, trim($_POST['email']));
-	}
-
 	// Check for a password and match against the confirmed password:
 	if (!empty($_POST['pass1'])) {
 		if ($_POST['pass1'] != $_POST['pass2']) {
@@ -40,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		// Register the user in the database...
 		
 		// Make the query:
-		$q = "INSERT INTO user (username, email, password, join_date) VALUES ('$fn', '$e', SHA1('$p'), NOW() )";		
+		$q = "INSERT INTO admin (name, password) VALUES ('$fn', SHA1('$p'))";		
 		$r = @mysqli_query ($dbc, $q); // Run the query.
 		if ($r) { // If it ran OK.
 		
@@ -78,9 +71,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 } // End of the main Submit conditional.
 ?>
-<form action="register_script.php" method="post">
+<form action="admin_register_script.php" method="post">
 	<p>Username: <input type="text" name="username" size="15" maxlength="20" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>" /></p>
-	<p>Email Address: <input type="text" name="email" size="20" maxlength="40" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" /> </p>
 	<p>Password: <input type="password" name="pass1" size="10" maxlength="40" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>" /></p>
 	<p>Confirm Password: <input type="password" name="pass2" size="10" maxlength="20" value="<?php if (isset($_POST['pass2'])) echo $_POST['pass2']; ?>" /></p>
 	<p><input type="submit" name="submit" value="Register" /></p>
