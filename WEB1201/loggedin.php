91b6f37028f955554379c3ba662dc6fee9e2bcdd
YKTan0303 <139303783+YKTan0303@users.noreply.github.com>
2023-12-12 01:12:48 +0000

    delete

--- WEB1201/loggedin.php
+++
@@ -1,8 +0,0 @@
-<h1>LOG IN SUCCESSFUL</h1>
-<?php
-    session_start();
-    echo $_SESSION['user_id'];
-    echo $_SESSION['username'];
-    echo $_SESSION['admin_id'];
-    echo $_SESSION['name'];
-?>
